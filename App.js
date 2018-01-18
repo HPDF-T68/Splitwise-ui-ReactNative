08180ffd9a109536a85c77727b6ac31a170c00b8
niyasns <niyasns007@outlook.com>
2018-01-18 23:23:50 +0530

    Login screen ui completed

--- App.js
+++ App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import Login from './src/screens/login';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
+        <Login />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/login.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+import { Button, Text } from 'native-base';
+
+export default class Login extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <View>
+                    <Image
+                        resizeMethod='auto'
+                        style={styles.logo}
+                        source={require('../img/splitwise_logo.png')}
+                    />
+                    <Button 
+                        light
+                        style={styles.login}
+                        resizeMethod='auto'
+                    >
+                        <Text style={styles.loginText}>Log in</Text>
+                    </Button>
+                    <Button 
+                        light
+                        style={styles.signup}
+                        resizeMethod='auto'
+                    >
+                        <Text style={styles.loginText}>Sign Up</Text>
+                    </Button>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 5,
+    },
+    logo: {
+        alignSelf: 'center',
+        marginTop: 20,
+        width: 300,
+        height: 300
+    },
+    login: {
+        marginTop: 30,
+        alignSelf: 'center',
+        width: 300,
+        height: 50,
+        backgroundColor: '#B0BEC5',
+        justifyContent: 'center',
+    },
+    signup: {
+        marginTop: 15,
+        alignSelf: 'center',
+        width: 300,
+        height: 50,
+        backgroundColor: '#69F0AE',
+        justifyContent: 'center',
+    },
+    loginText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'white',
+    }
+});
